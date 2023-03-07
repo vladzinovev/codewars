e3efffe1efbd65c9b7d6184959e51eb62511a7d2
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-03-07 19:14:20 +0300

    added a short solution

--- multiplication_table.js
+++ multiplication_table.js
@@ -27,3 +27,15 @@ console.log(multiplicationTable(1))//[[1]]
 console.log(multiplicationTable(2))//[[1,2],[2,4]]
 console.log(multiplicationTable(3))//[[1,2,3],[2,4,6],[3,6,9]]
 console.log(multiplicationTable(4))//[[1,2,3,4],[2,4,6,8],[3,6,9,12],[4,8,12,16]]
+
+//короткий вариант
+multiplicationTable = function(size) {
+    var result = [];
+    for (var i = 0; i < size; i++) {
+      result[i] = [];
+      for(var j = 0; j < size; j++) {
+        result[i][j] = (i + 1) * (j + 1);
+      }
+    }
+    return result
+}
