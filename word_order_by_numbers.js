f5e07f4bfadb4c06ef2ef9671008eafcd4e7f0e0
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-03-15 21:09:42 +0300

    added word order by nymbers

---
+++ word_order_by_numbers.js
@@ -0,0 +1,23 @@
+function order(words) {
+  if (!words) return "";
+  let arr = words.split(" ");
+  arr.unshift("empty");
+  let arrNew = [];
+  let index = 1;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (index == arr[i][j]) {
+        arrNew.push(arr[i]);
+        index++;
+        i = 0;
+      }
+    }
+  }
+  return arrNew.join(" ");
+}
+console.log(order("3a Thi1s is2 T4est"));
+//order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
+//order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
+//order(""), "", "empty input should return empty string" )
+
+
