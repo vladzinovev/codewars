dd02d425910be70cf95d4aac595c7a943b721ed5
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-05-05 23:45:13 +0300

    added a short solution

--- 4kyu/roman_numerals_helper.js
+++ 4kyu/roman_numerals_helper.js
@@ -118,8 +118,25 @@ function fromRoman(str) {
 }
 console.log(fromRoman('IV'))
  
- 
- 
+//short solution
+const map = { M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1};
+
+class RomanNumerals {
+    static toRoman(num) {
+        let str = '';
+        for (var i in map) {
+            while (num >= map[i]) {
+                str += i;
+              num -= map[i];
+            }
+        }
+        return str;
+    }
+    
+    static fromRoman(str) {
+         return str.match(/CM|CD|XC|XL|IX|IV|\w/g).reduce((acc, el) => acc + map[el], 0);
+    }
+}
  
  
  
