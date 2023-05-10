e7b0ea940d1c568b32a2864ff0126afa17a556b1
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-05-10 23:32:43 +0300

    added valid parenthese

---
+++ 7kyu/valid_parentheses.js
@@ -0,0 +1,12 @@
+function validParentheses(parenStr) {
+    const arr=parenStr.split('');
+    let right=0, left=0,result;
+    for(i=0;i<arr.length;i++){
+        arr[i]=='('?left++:right++;
+      if(right>left){
+        return false
+      }
+    }
+    if(right==left){return true}else{return false};
+}
+
