8d0e1b6e2eb0dc6bc496df616856af8c6e29caf3
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-05-12 01:37:52 +0300

    added new solutione

--- 5kyu/moving_zeros_to_end.js
+++ 5kyu/moving_zeros_to_end.js
@@ -16,4 +16,9 @@ function moveZeros(arr) {
     }
     return arrNew;
   }
-  //console.log(moveZeros([false,1,0,1,2,0,1,3,"a"])) // returns[false,1,1,2,1,3,"a",0,0]
+//console.log(moveZeros([false,1,0,1,2,0,1,3,"a"])) // returns[false,1,1,2,1,3,"a",0,0]
+
+//short solution
+var moveZeros = function (arr) {
+  return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
+}
