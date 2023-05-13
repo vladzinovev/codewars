bd45c96982bed073bba6535f99fb312d480538db
Vladislav  Zinovyev <vladozdeveloper@gmail.com>
2023-05-14 00:23:00 +0300

    rewrited code

--- 6kyu/playing_with_digits.js
+++ 6kyu/playing_with_digits.js
@@ -1,26 +1,21 @@
 function digPow(n, p){
-    console.log(n,p)
     let arr=n.toString().split('');
-    let p2=p;
-    let sum=0;
+    let p2=p, sum=0;
     for (i=0;i<arr.length;i++){
       sum+=Math.pow(Number(arr[i]),p2);
       p2++;
     }
-    console.log(sum)
-    
-    if(sum<n*p){
-      return -1
-    }
-    if(sum>=n*p){
+    if(sum==n*p){
+      return p
+    }else{
       if(sum%n==0){
         return sum/n
       }else{
-          return -1
-        }
+        return -1
+      }
     }
-    
-  }
+}
+
 //(digPow(89, 1), 1)
 //(digPow(92, 1), -1)
 //(digPow(46288, 3), 51)
